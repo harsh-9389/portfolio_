034e4bc0e7797228421a3b839fa5790490214b66
Harsh <99734334+harsh-9389@users.noreply.github.com>
2025-01-05 00:26:33 +0530

    started

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return (
+        <nav>
+            <a href="/">Introduction</a>
+            <a href="/about">Skills</a>
+            <a href="/projects">Projects</a>
+            <a href="/blog">Resume</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    )
+}
